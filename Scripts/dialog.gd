13d11e2c414b808180f22dd7da0f9f282b80e32e
Corentin <corentinboblet@gmail.com>
2024-10-08 16:31:11 +0200

    Dialog management

---
+++ Scripts/dialog.gd
@@ -0,0 +1,7 @@
+extends Resource
+class_name Dialog
+
+@export var name : String;
+@export var text : String;
+@export var visual_left : Texture2D;
+@export var visual_right : Texture2D;
---
+++ Scripts/dialog_manager.gd
@@ -0,0 +1,56 @@
+extends Control
+
+@export_category("Nodes")
+@export var name_box : Label;
+@export var dialog_box : Label;
+@export var visual_left : TextureRect;
+@export var visual_right : TextureRect;
+
+@export_category("Dialogs")
+@export var dialogs : Array[Dialog];
+
+@export_category("Parameters")
+@export var keyboard_typing_duration : float;
+
+var current_dialog_index : int = 0;
+var current_dialog : Dialog;
+
+var dialog_tween : Tween;
+
+func _ready() -> void:
+	if(dialogs.is_empty()):
+		push_error("no dialogs");
+		return;
+	
+	dialog_tween = get_tree().create_tween();
+	current_dialog = dialogs[current_dialog_index];
+	update_dialog();
+
+func _input(event: InputEvent) -> void:
+	if(event.is_action_released("ui_accept")):
+		next_dialog();
+
+func next_dialog():
+	if(dialog_tween.is_running()):
+		dialog_tween.kill();
+		dialog_box.visible_ratio = 1;
+		return;
+	
+	if(current_dialog_index >= dialogs.size()-1):
+		print("no more dialogs");
+		return;
+	
+	current_dialog_index+=1;
+	current_dialog = dialogs[current_dialog_index];
+	update_dialog();
+
+func update_dialog():
+	name_box.text = current_dialog.name;
+	dialog_box.text = current_dialog.text;
+	visual_left.texture = current_dialog.visual_left;
+	visual_right.texture = current_dialog.visual_right;
+	
+	if(!dialog_tween.is_valid()):
+		dialog_tween = get_tree().create_tween();
+	dialog_box.visible_ratio = 0;
+	dialog_tween.tween_property(dialog_box, "visible_ratio", 1, keyboard_typing_duration);
